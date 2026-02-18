f89baa303b1e8c678492d7b00b5b8a760baab4ed
Sithum Imesh <si@tid.global>
2026-02-18 11:22:00 +0530

    bug fixed

--- src/components/layout/VideoBackground.jsx
+++ src/components/layout/VideoBackground.jsx
@@ -6,6 +6,7 @@ const VideoBackground = () => {
       autoPlay
       loop
       muted
+      playsInline
       className="object-cover fixed top-0 left-0 w-full h-full opacity-30 -z-10"
     >
       <source src="/backgroud.mp4" type="video/mp4" />
